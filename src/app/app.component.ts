039ced5400a890e25b177318e95c01fec9725510
MarkAWulf <mark.wulf@gmail.com>
2025-05-22 23:01:35 -0500

    changes to form added bootstrap changed to show long date and formated currency.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'pipes';
+name: string = '';  
+date: string = '';
+amount: number = 0;
+
+onNameChange(event: Event): void {
+  const input = event.target as HTMLInputElement;
+  this.name = input?.value ?? '';
+  console.log('User typed:', this.name);
+}
+
+onDateChange(event: Event): void {
+  const inputdate = event.target as HTMLInputElement;
+  this.date = inputdate?.value?? '';
+  console.log('User typed:', this.date )
+}
+
+onAmountChange(event: Event): void {
+  const amount = event.target as HTMLInputElement;
+  this.amount = parseFloat(amount?.value?? '');
+  console.log('User typed:', this.amount )
+}
 }
